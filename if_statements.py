696c97c2ae942dc844c7b0be51961c37b7eb969f
GZMaster <retrodevstechnology@gmail.com>
2024-09-08 05:43:27 +0100

    list comprehension

--- if_statements.py
+++ if_statements.py
@@ -1,33 +1,33 @@
-should_continue = True
+# should_continue = True
 
-if should_continue:
-    print("Hello")
+# if should_continue:
+#     print("Hello")
     
     
     
     
     
-known_people = ["John", "Anna", "Mary"]
+# known_people = ["John", "Anna", "Mary"]
 
-person = input("Enter the person you know: ")
+# person = input("Enter the person you know: ")
 
-if person in known_people:
-    print("You know {}!".format(person))
-else:
-    print("You don't know {}!".format(person))
+# if person in known_people:
+#     print("You know {}!".format(person))
+# else:
+#     print("You don't know {}!".format(person))
     
 
 def who_do_you_know():
     people = input("Enter the names of people you know, separated by commas: ")
-    people_list = people.split(",")
-    
-    people_without_spaces = []
-    for person in people_list:
-        people_without_spaces.append(person.strip())
+    people_without_spaces = [person.strip() for person in people.split(",")]
     return people_without_spaces
 
 
 def ask_user():
     person = input("Enter the name of someone you know: ")
-    return person
+    if person in who_do_you_know():
+        print("You know {}!".format(person))
+    else:
+        print("You don't know {}!".format(person))
   
+ask_user()
---
+++ list_comprehension.py
@@ -0,0 +1,17 @@
+my_list = [1, 3, 5, 7, 9]
+an_equal_list = [x * 2 for x in range(5)]
+
+print(an_equal_list)
+
+multiplied_list = [x * 2 for x in range(5)]
+
+print(multiplied_list)
+
+print(5 % 3)
+
+print([n for n in range(10) if n % 2 == 0 =])
+
+people_you_know = ["Rolf", "John", "anna", "GREG"]
+normalized_people = [person.strip().lower() for person in people_you_know]
+
+print(normalized_people)
