0d45c23e899e21b16abe71514d22d66501a39457
GZMaster <retrodevstechnology@gmail.com>
2024-09-08 05:33:22 +0100

    if statement

--- if_statements.py
+++ if_statements.py
@@ -17,4 +17,17 @@ else:
     print("You don't know {}!".format(person))
     
 
+def who_do_you_know():
+    people = input("Enter the names of people you know, separated by commas: ")
+    people_list = people.split(",")
+    
+    people_without_spaces = []
+    for person in people_list:
+        people_without_spaces.append(person.strip())
+    return people_without_spaces
+
+
+def ask_user():
+    person = input("Enter the name of someone you know: ")
+    return person
   
