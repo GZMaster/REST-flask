ec7e174e49832f83fa11f96e5fd4fec70415e003
GZMaster <retrodevstechnology@gmail.com>
2024-09-08 04:21:08 +0100

    introduction

---
+++ vairable_methods.py
@@ -0,0 +1,19 @@
+var1 = "Hello World"
+var2 = "Python Programming"
+
+num1 = 8
+num2 = 2
+
+print(num1 * num2)
+
+##
+
+def my_print_method(my_argument):
+    print(my_argument)
+
+my_print_method("Hello World")
+
+def my_multiply_method(number_one, number_two):
+    return number_one * number_two
+
+print(my_multiply_method(5, 3))
