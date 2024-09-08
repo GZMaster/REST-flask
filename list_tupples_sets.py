deadf59db8bd8c00fb236f400925a06c04ee7677
GZMaster <retrodevstechnology@gmail.com>
2024-09-08 04:39:49 +0100

    intro set tuple and list

---
+++ list_tupples_sets.py
@@ -0,0 +1,21 @@
+my_variable = "Hello World"
+
+grade_one = 90
+grade_two = 80
+grade_three = 70
+
+grades = [90, 80, 70]
+
+print(grades[0])
+
+grades.append(60)
+
+tuple_grades = (90, 80, 70)
+
+set_grades = {90, 80, 70, 60, 50, 40, 30, 20, 10}
+
+print(set_grades)
+
+set_grades.add(55)
+
+print(set_grades)
