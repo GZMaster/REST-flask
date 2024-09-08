b3542db8ce50bda193ac9458cb80be93f9cc75ab
GZMaster <retrodevstechnology@gmail.com>
2024-09-08 04:27:20 +0100

    method exercise

---
+++ method_example.py
@@ -0,0 +1,16 @@
+
+
+def return_42():
+    return 42
+
+print(return_42())
+
+def my_method(num1, num2):
+    return num1 + num2
+
+print(my_method(5, 3))
+
+def addition_method(num1, num2):
+    return num1 + num2
+
+print(addition_method(5, 3))
