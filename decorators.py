a84b4a3f2e4097769a0eabe8413c08564e7cf195
GZMaster <retrodevstechnology@gmail.com>
2024-09-08 18:41:41 +0100

    advance decorators

--- decorators.py
+++ decorators.py
@@ -12,4 +12,22 @@ def my_decorator(func):
 def my_function():
   print("I'm the function!")
   
-my_function()
+# my_function()
+
+def decorator_with_arguments(number):
+  @functools.wraps(func) 
+  def function_that_runs_func(*args, **kwargs):
+    print("In the decorator!")
+    if number == 56:
+      print("Not running the function")
+    else:
+      func(*args, **kwargs)
+    print("After the decorator!")
+  return function_that_runs_func
+
+@decorator_with_arguments(56)
+def my_function_too():
+  print("Hello")
+  
+my_function_too()
+
