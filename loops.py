c0bd9965c66306ff4b690f4470b979fa75d7adac
GZMaster <retrodevstechnology@gmail.com>
2024-09-08 05:09:09 +0100

    intro loop

---
+++ loops.py
@@ -0,0 +1,15 @@
+my_variable = "Hello World"
+
+for char in my_variable:
+  print(char)
+  
+  my_list = [1, 2, 3, 4, 5]
+  
+  for number in my_list:
+    print(number * 2)
+    
+    
+    
+    
+    
+  
