0787705648023308f55d060cdb6c8acd0c25fe99
GZMaster <retrodevstechnology@gmail.com>
2024-09-08 05:51:24 +0100

    dictionary

---
+++ dictionary.py
@@ -0,0 +1,20 @@
+my_set = {1, 2, 3, 4, 5}
+my_dict = {"name": "John", "age": 20, "city": "New York"}
+another_dict = dict(name="John", age=20, city="New York")
+
+lottery_player_dict = {
+    "name": "Rolf",
+    "numbers": (5, 9, 12, 3, 1, 21)
+}
+
+universities = [
+    {
+        
+    }
+]
+
+sum(lottery_player_dict["numbers"])
+
+lottery_player_dict["name"] = "John"
+
+print(lottery_player_dict)
