83caa18c767024801f453fc57a9ffd68e04651d2
GZMaster <retrodevstechnology@gmail.com>
2024-09-13 14:31:55 +0100

    store REST implementation

--- app.py
+++ app.py
@@ -1,11 +1,68 @@
-from flask import Flask
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
+stores = [
+    {
+        "name": "My Wonderful Store",
+        "items": [
+            {
+                "name": "My Item",
+                "price": 15.99,
+            }
+        ],
+    },
+    {
+        "name": "My Wonderful Store 2",
+        "items": [
+            {
+                "name": "My Item 2",
+                "price": 15.99,
+            }
+        ],
+    },
+]
 
-@app.route("/")
-def index():
-    return "Hello, World!"
+
+@app.route("/store", methods=["POST"])
+def create_store():
+    request_data = request.get_json()
+    new_store = {"name": request_data["name"], "items": []}
+
+    stores.append(new_store)
+    return jsonify(new_store)
 
 
-app.run(port=5000)
+@app.route("/store/<string:name>")
+def get_store(name):
+    for store in stores:
+        if store["name"] == name:
+            return jsonify(store)
+    return jsonify({"message": "store not found"})
+
+
+@app.route("/store")
+def get_stores():
+    return jsonify({"stores": stores})
+
+
+@app.route("/store/<string:name>/item", methods=["POST"])
+def create_item_in_store(name):
+    request_data = request.get_json()
+    for store in stores:
+        if store["name"] == name:
+            new_item = {"name": request_data["name"], "price": request_data["price"]}
+            store["items"].append(new_item)
+            return jsonify(new_item)
+    return jsonify({"message": "store not found"})
+
+
+@app.route("/store/<string:name>/item")
+def get_items_in_store(name):
+    for store in stores:
+        if store["name"] == name:
+            return jsonify({"items": store["items"]})
+    return jsonify({"message": "store not found"})
+
+
+app.run(port=6000)
